ec3181538d14caa761d9a51f53dfbd2638e6f126
Amir goodarzi <amirgoodarzi.web@gmail.com>
2022-05-27 16:38:49 +0430

    optimize images -- responsive problems

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -10,35 +10,29 @@ import Title from "components/Title/Title";
 //============================================
 function AboutMe() {
   return (
-    <div className={styles.wrapper}>
-      <div className="max_width_rule flex">
-        <div className={styles.descriptionWrapper}>
-          <div className={styles.description}>
-            <Title
-              back="AboutMe"
-              front={{ normal: "Me,MySelf &", bold: "I" }}
-            />
-            <p>
-              I’m a Front-End & Mobile Developer located in Iran. Bachelor of
-              Computer Engineering graduate and software student in M.Sc.
-              <br />I have a serious passion for UI effects, animations and
-              creating intuitive, dynamic user experiences. Well-organised
-              person, problem solver, independent employee with attention to
-              detail.
-            </p>
-            <p>
-              Fan of Video Games, outdoor activities and Persian rap Music.
-              <br />A person , Interested in the entire frontend spectrum and
-              working on ​projects with positive people.
-            </p>
-            <a href="/assets/files/NewResumeFile.pdf" download>
-              <Button type="FILL" text="DOWNLOAD CV" />
-            </a>
-          </div>
+    <div className={`max_width_rule ${styles.wrapper}`}>
+      <div className={styles.descriptionWrapper}>
+        <div className={styles.description}>
+          <Title back="AboutMe" front={{ normal: "Me,MySelf &", bold: "I" }} />
+          <p>
+            I’m a Front-End & Mobile Developer located in Iran. Bachelor of
+            Computer Engineering graduate and software student in M.Sc.
+            <br />I have a serious passion for UI effects, animations and
+            creating intuitive, dynamic user experiences. Well-organised person,
+            problem solver, independent employee with attention to detail.
+          </p>
+          <p>
+            Fan of Video Games, outdoor activities and Persian rap Music.
+            <br />A person , Interested in the entire frontend spectrum and
+            working on ​projects with positive people.
+          </p>
+          <a href="/assets/files/NewResumeFile.pdf" download>
+            <Button type="FILL" text="DOWNLOAD CV" />
+          </a>
         </div>
-        <div className={styles.aboutMeImage}>
-          <img src="/assets/images/person.jpg" alt="AmirHossein Goodarzi" />
-        </div>
+      </div>
+      <div className={styles.aboutMeImage}>
+        <img src="/assets/images/person.png" alt="AmirHossein Goodarzi" />
       </div>
     </div>
   );
--- components/Code/Code.js
+++ components/Code/Code.js
@@ -6,6 +6,7 @@ import { Download } from "react-bootstrap-icons";
 //============== ex components ===============
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import { codeTheme } from "./code.style";
+import toast from "react-hot-toast";
 //================= redux ====================
 
 function Code({ children, file }) {
@@ -35,15 +36,28 @@ function Code({ children, file }) {
         link.parentNode.removeChild(link);
       });
   };
+  const copy = () => {
+    navigator.clipboard.writeText(children.toString());
+    toast.success("Code copied to clipboard!");
+  };
   return (
     <div className="relative">
       <button
-        href={file}
         className="absolute -right-3 -top-0 bg-main bg-opacity-60 p-2 rounded-md"
         onClick={downloadFile}
       >
         <Download />
       </button>
+      <svg
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        className="absolute -right-3 -bottom-0 bg-main bg-opacity-60 rounded-md w-8 h-8 flex justify-center items-center cursor-pointer"
+        onClick={copy}
+      >
+        <path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path>
+        <path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path>
+      </svg>
       <SyntaxHighlighter showLineNumbers language="jsx" style={codeTheme}>
         {children}
       </SyntaxHighlighter>
