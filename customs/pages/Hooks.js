08cf8233742781b40bb2df79ecc9d2f55f264098
Amir goodarzi <amirgoodarzi.web@gmail.com>
2022-06-10 00:32:57 +0430

    add useToggleCustomHook

--- customs/pages/Hooks.js
+++ customs/pages/Hooks.js
@@ -1,26 +1,91 @@
 //============== React & Next ================
-import React from "react";
+import React, { useState } from "react";
 //============== images & icons ==============
 //============== in Components ===============
 import HookViewer from "components/HookViewer/HookViewer";
 import { useNetworkStatus } from "customs/hooks/useNetworkStatus";
+import { useToggle } from "customs/hooks/useToggle";
+import FloatingInput from "customs/components/FloatingInput/FloatingInput";
 //============== ex Components ===============
 //================= redux ====================
 //============================================
+
 function Hooks() {
+  const [searchInput, setSearchInput] = useState({
+    value: "",
+    error: "",
+  });
+
   const [isOnline, connection] = useNetworkStatus();
+  const [theme, toggleTheme] = useToggle('dark', ['dark', 'light']);
+
+  const hooks_list = [
+    {
+      usage: `const [isOnline, connection] = useNetworkStatus();
+
+<p>{isOnline ? "Online" : "Offline"}</p>
+<p>Down Link : {connection.downlink}</p>
+<p>effective type : {connection.effectiveType}</p>
+<p>latency : {connection.rtt}</p>
+<p>data saver? : {connection.saveData ? "Yes" : "No"}</p>`,
+      description: `A hook for get user network status`,
+      file: `useNetworkStatus`,
+      children: (
+        <>
+          <p>{isOnline ? "Online" : "Offline"}</p>
+          <p>Down Link : {connection.downlink}</p>
+          <p>effective type : {connection.effectiveType}</p>
+          <p>latency : {connection.rtt}</p>
+          <p>data saver? : {connection.saveData ? "Yes" : "No"}</p>
+        </>
+      ),
+    },
+    {
+      usage: `const [theme, toggleTheme] = useToggle('dark', ['dark', 'light']);
+
+<h1>{`+"`Theme: ${theme}`"+`}</h1>
+<button onClick={() => toggleTheme()}>Toggle!</button>`,
+      description: `A hook for get user network status`,
+      file: `useToggle`,
+      children: (
+        <>
+            <h1 className="text-2xl my-2">{`Theme: ${theme}`}</h1>
+            <button onClick={() => toggleTheme()} className="p-2 my-2 bg-main text-dark rounded-md font-bold">Toggle theme</button>
+        </>
+      ),
+    },
+  ];
   return (
     <>
-      <h1>{isOnline ? "Online" : "Offline"}</h1>
-      <h1>Down Link : {connection.downlink}</h1>
-      <h1>effective type : {connection.effectiveType}</h1>
-      <h1>latency : {connection.rtt}</h1>
-      <h1>data saver? : {connection.saveData?"Yes":"No"}</h1>
-      <HookViewer
-        source={`this is source of my first hook`}
-        usage={`usage`}
-        file={"useNetworkStatus"}
+      <FloatingInput
+        label="Search by name"
+        value={searchInput.value}
+        error={searchInput.error}
+        onChange={(e) => {
+          setSearchInput({
+            ...searchInput,
+            value: e.target.value,
+          });
+        }}
+        wrapperClassNames="!w-3/4 lg:!w-1/2 !m-10"
+        required={true}
       />
+      {hooks_list
+        .filter((hook) =>
+          hook.file.toLowerCase().includes(searchInput.value.toLowerCase())
+        )
+        .map((hook, index) => {
+          return (
+            <HookViewer
+              key={index}
+              usage={hook.usage}
+              description={hook.description}
+              file={hook.file}
+            >
+              {hook.children}
+            </HookViewer>
+          );
+        })}
     </>
   );
 }
