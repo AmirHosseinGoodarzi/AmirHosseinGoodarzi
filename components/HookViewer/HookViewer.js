8b1ecc3be7051664e3bcce20cc48313846003140
Amir goodarzi <amirgoodarzi.web@gmail.com>
2022-06-10 00:31:45 +0430

    complete hookViewer and customHooks page

--- components/HookViewer/HookViewer.js
+++ components/HookViewer/HookViewer.js
@@ -1,8 +1,8 @@
 //============== React & Next ================
-import React, { useState } from "react";
-import styles from "./HookViewer.module.scss";
+import React, { useEffect, useState } from "react";
+import styles from "../ComponentViewer/ComponentViewer.module.scss";
 //============== images & icons ==============
-import { Activity, CodeSlash, Download } from "react-bootstrap-icons";
+import { CodeSlash, Eye } from "react-bootstrap-icons";
 //============== in components ===============
 //============== ex components ===============
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
@@ -10,23 +10,16 @@ import { theme } from "../ComponentViewer/SyntaxHighlighter.style";
 import toast from "react-hot-toast";
 //================= redux ====================
 
-function HookViewer({ source, usage, file }) {
-  const [showSource, setShowSource] = useState(false);
-  const downloadFile = () => {
-    const baseUrl =
-      "https://raw.githubusercontent.com/AmirHosseinGoodarzi/AmirHosseinGoodarzi/main/customs/hooks/";
-    fetch(baseUrl + `${file}.js`, { method: "GET" })
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", `${file}.js`);
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-      });
-  };
+const TABS = Object.freeze({
+  SOURCE: "SOURCE",
+  PREVIEW: "PREVIEW",
+});
+const baseUrl =
+  "https://raw.githubusercontent.com/AmirHosseinGoodarzi/AmirHosseinGoodarzi/main/customs/hooks/";
+
+function HookViewer({ source, usage, file, description, children }) {
+  const [activeTab, setActiveTab] = useState(TABS.PREVIEW);
+  const [sourceCode, setSourceCode] = useState("");
   const copySource = () => {
     navigator.clipboard.writeText(source);
     toast.success("Source copied to clipboard!");
@@ -35,22 +28,36 @@ function HookViewer({ source, usage, file }) {
     navigator.clipboard.writeText(usage);
     toast.success("Hook Usage copied to clipboard!");
   };
+  const readSourceCode = () => {
+    fetch(`${baseUrl}${file}.js`).then((response) => {
+      response.text().then((text) => {
+        setSourceCode(text);
+      });
+    });
+  };
+  useEffect(() => {
+    readSourceCode();
+  }, []);
+
   return (
-    <div className={styles.componentViewer_wrapper}>
+    <div className={styles.hookViewer_wrapper}>
       <ul className={styles.tab_list}>
+        <div>
+          <h1 className="text-2xl text-main mb-3">{file}</h1>
+        </div>
         <li
-          className={showSource ? "" : styles.active}
+          className={activeTab === TABS.PREVIEW ? styles.active : ""}
           onClick={() => {
-            setShowSource(false);
+            setActiveTab(TABS.PREVIEW);
           }}
         >
-          <Activity className="mr-1" />
-          <span>Usage</span>
+          <Eye className="mr-1" />
+          <span>Preview</span>
         </li>
         <li
-          className={showSource ? styles.active : ""}
+          className={activeTab === TABS.SOURCE ? styles.active : ""}
           onClick={() => {
-            setShowSource(true);
+            setActiveTab(TABS.SOURCE);
           }}
         >
           <CodeSlash className="mr-1" />
@@ -60,26 +67,40 @@ function HookViewer({ source, usage, file }) {
       <div className={styles.content_wrapper}>
         <div className={styles.code_wrapper}>
           <div className={styles.tools}>
-            {showSource && (
-              <button className={styles.download_btn} onClick={downloadFile}>
-                <Download />
-              </button>
-            )}
             <svg
               fill="none"
               stroke="currentColor"
               strokeWidth="1.5"
               className={styles.copy_btn}
-              onClick={showSource ? copySource : copyUsage}
+              onClick={activeTab === TABS.SOURCE ? copySource : copyUsage}
             >
               <path d="M13 10.75h-1.25a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H19"></path>
               <path d="M18 12.25h-4a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM13.75 16.25h4.5M13.75 19.25h4.5"></path>
             </svg>
           </div>
           <div className="relative w-full">
-            <SyntaxHighlighter showLineNumbers language="jsx" style={theme}>
-              {showSource ? source : usage}
-            </SyntaxHighlighter>
+            {activeTab === TABS.PREVIEW ? (
+              <div className="flex flex-wrap justify-center items-center">
+                <div className="w-full lg:w-1/2 my-5 lg:my-0 p-0 lg:pr-4">
+                  Description : {description}
+                  <hr className="my-3" />
+                  {children}
+                </div>
+                <div className="w-full lg:w-1/2">
+                  <SyntaxHighlighter
+                    showLineNumbers
+                    language="jsx"
+                    style={theme}
+                  >
+                    {usage}
+                  </SyntaxHighlighter>
+                </div>
+              </div>
+            ) : (
+              <SyntaxHighlighter showLineNumbers language="jsx" style={theme}>
+                {sourceCode}
+              </SyntaxHighlighter>
+            )}
           </div>
         </div>
       </div>
