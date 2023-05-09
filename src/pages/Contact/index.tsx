4ac817582b976e5af33d8ef50b9accc5642fbac2
AmirHossein Goodarzi <amirgoodarzi.web@gmail.com>
2023-05-09 16:01:17 +0430

    contact form error handling and form submiting

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -3,6 +3,7 @@ import "./contact.scss";
 import { useForm } from "react-hook-form";
 import AbsoluteShape from "~/components/AbsoluteShape";
 import { ReactComponent as Linkedin } from "~/assets/images/icons/linkedin.svg";
+import { ReactComponent as Github } from "~/assets/images/icons/github.svg";
 import { ReactComponent as StackOverFlow } from "~/assets/images/icons/stackoverflow.svg";
 import { ReactComponent as Telegram } from "~/assets/images/icons/telegram.svg";
 const Contact = () => {
@@ -12,7 +13,10 @@ const Contact = () => {
     formState: { errors },
   } = useForm();
   const formSubmiteHandler = handleSubmit((data) => {
-    console.log({ data });
+    const aTag = document.createElement("a");
+    aTag.href =
+      `mailto:amirgoodarzi.web@gmail.com?subject=This is a message from ${data.email} - ${data.name} on your CV.&body=${data.message}`;
+    aTag.click();
   });
   return (
     <div className="container">
@@ -30,7 +34,12 @@ const Contact = () => {
                 href="https://www.linkedin.com/in/amir-hossein-goodarzi/"
                 target="_blank"
               >
-                <Linkedin />
+                <Linkedin className="w-7 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/AmirHosseinGoodarzi" target="_blank">
+                <Github className="w-7 h-auto" />
               </a>
             </li>
             <li>
@@ -38,12 +47,12 @@ const Contact = () => {
                 href="https://stackoverflow.com/users/13922873/sektowr"
                 target="_blank"
               >
-                <StackOverFlow />
+                <StackOverFlow className="w-7 h-auto" />
               </a>
             </li>
             <li>
               <a href="https://t.me/sektowr" target="_blank">
-                <Telegram />
+                <Telegram className="w-7 h-auto" />
               </a>
             </li>
           </ul>
@@ -51,32 +60,51 @@ const Contact = () => {
         </div>
         <div className="form_wrapper">
           <div>
+            <input
+              className={`form_input ${errors.name ? "invalid" : ""}`}
+              placeholder=" "
+              {...register("name", { required: true })}
+            />
             <label htmlFor="first-name" className="form_label">
               Your Name
             </label>
-            <input
-              className="form_input"
-              {...register("name", { required: true })}
-            />
+            {errors.name && <p className="error_message">Name is required</p>}
           </div>
           <div>
+            <input
+              className={`form_input ${errors.email ? "invalid" : ""}`}
+              placeholder=" "
+              {...register("email", {
+                required: true,
+                pattern: /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/,
+              })}
+            />
             <label htmlFor="email" className="form_label">
               Email
             </label>
-            <input
-              className="form_input"
-              {...register("email", { required: true })}
-            />
+            {errors.email ? (
+              <p className="error_message">
+                {errors.email.type === "required"
+                  ? "Email address is required"
+                  : "Enter a valid email address please."}
+              </p>
+            ) : (
+              <></>
+            )}
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="form_label">
-              Message
-            </label>
             <textarea
-              className="form_input"
+              className={`form_input ${errors.message ? "invalid" : ""}`}
+              placeholder=" "
               rows={4}
               {...register("message", { required: true })}
             />
+            <label htmlFor="message" className="form_label">
+              Message
+            </label>
+            {errors.message && (
+              <p className="error_message">Message is required</p>
+            )}
           </div>
           <div className="sm:col-span-2 mt-2.5">
             <Button className="!w-full !py-2" onClick={formSubmiteHandler}>
